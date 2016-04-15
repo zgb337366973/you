00aba9100a5333efa85108094a7e1b9cfa1f5507
xmg <xmg@163.com>
2016-04-15 10:39:17 +0800

    dddcd

---
+++ ni/ni/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ni
+//
+//  Created by zgb13790090648 on 16/4/15.
+//  Copyright © 2016年 zgb13790090648. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ni/ni/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ni
+//
+//  Created by zgb13790090648 on 16/4/15.
+//  Copyright © 2016年 zgb13790090648. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
